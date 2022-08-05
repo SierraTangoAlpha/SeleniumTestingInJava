7de8f58d902894456435bc940acbab5170ce6a4e
SierraTangoAlpha <33183694+SierraTangoAlpha@users.noreply.github.com>
2022-08-05 13:38:16 +0200

    Add files via upload

---
+++ RegistrationPrefilledTest.java
@@ -0,0 +1,63 @@
+package base;
+import pages.*;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+
+public class RegistrationPrefilledTest extends BaseTests {
+
+    @Test
+    public void testGettingThere () {
+
+        RegistrationPage registrationPage = intermediatePage.fillEmailAndClick();
+
+        //Fill forms
+        registrationPage.fillFirstNameCustomer("SomeFirstName");
+        assertEquals(registrationPage.getFirstNameCustomer(), "SomeFirstName");
+
+        registrationPage.fillLastNameCustomer("SomeLastName");
+        assertEquals(registrationPage.getLastNameCustomer(), "SomeLastName");
+
+        registrationPage.fillPasswordCustomer("aGenericPassword");
+        assertEquals(registrationPage.getPasswordCustomer(), "aGenericPassword");
+
+
+        assertEquals(registrationPage.getFirstName(), "SomeFirstName");
+        assertEquals(registrationPage.getLastName(), "SomeLastName");
+
+
+        registrationPage.fillCompany("SomeCompany");
+        assertEquals(registrationPage.getCompany(), "SomeCompany");
+
+        registrationPage.fillAddress("SomeAddress");
+        assertEquals(registrationPage.getAddress(), "SomeAddress");
+
+        registrationPage.fillCity("SomeCity");
+        assertEquals(registrationPage.getCity(), "SomeCity");
+
+        registrationPage.fillPostCode(59800);
+        assertEquals(registrationPage.getPostcode(), "59800");
+
+        registrationPage.fillPhone(456845699);
+        assertEquals(registrationPage.getPhone(), "456845699");
+
+
+        //Choose and check
+        registrationPage.chooseGender("male");
+        assertTrue(registrationPage.isChecked("male"));
+
+        registrationPage.chooseDays();
+        assertEquals(registrationPage.daysList().size(), 1);
+        registrationPage.chooseMonths();
+        assertEquals(registrationPage.monthsList().size(), 1);
+        registrationPage.chooseYears();
+        assertEquals(registrationPage.yearsList().size(), 1);
+        registrationPage.chooseStates();
+        assertEquals(registrationPage.statesList().size(), 1);
+    }
+
+
+
+
+}
+
